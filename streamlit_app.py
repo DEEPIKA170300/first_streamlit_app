4b6eaede196fe5a4692eaf03bf08b9cd52dcb76e
DEEPIKA170300 <144422003+DEEPIKA170300@users.noreply.github.com>
2023-09-08 17:27:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,12 @@ streamlit.text(' 🥗 Kale,Spinach & Rocket smoothie')
 streamlit.text('🐔Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#python libary
 import pandas
+#pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list.
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
