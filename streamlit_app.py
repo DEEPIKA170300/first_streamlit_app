69cf5648a1105f63ce7d14cc5bd792ee22e60b51
DEEPIKA170300 <144422003+DEEPIKA170300@users.noreply.github.com>
2023-09-12 15:13:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,7 @@
 
 
 
-import streamlit
 
-
-streamlit.title("My Mom's New Healthy Diner")
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🥗 Kale,Spinach & Rocket smoothie')
-streamlit.text('🐔Hard-Boiled Free Range Egg')
-streamlit.text('🥑🍞Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-#python libary
-import pandas
-#pandas to read our CSV file from that S3 bucket so we use a pandas function called read_csv  to pull the data into a dataframe we'll call my_fruit_list.
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Let's put a pick list here so they can pick the fruit they want to include 
-#After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-import response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"fruit_choice")
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-
-
-import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
-
-add_my_friut=streamlit.text_input('What fruit would you like to add?')
-
-streamlit.write('Thanks for adding ',add_my_fruit)
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
 
 
